980f7407717bdb874b47e5e8727607999203810f
TylerGageElemind <Tyler.Gage@elemindtech.com>
2022-08-05 15:44:52 -0700

    Add in FreeRTOS
    Remove build outputs

---
+++ source/hardfault.c
@@ -0,0 +1,13 @@
+/*
+ * hardfault.c
+ *
+ *  Created on: Aug 5, 2022
+ *      Author: tyler
+ */
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask, char * pcTaskName ){
+  //ToDo: Implement
+}
