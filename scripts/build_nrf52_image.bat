dafcb961718b9cb76f75afc12d5462f1be938ba3
TylerGageElemind <Tyler.Gage@elemindtech.com>
2022-09-26 16:44:07 -0700

    Update build script

--- scripts/build_nrf52_image.bat
+++ scripts/build_nrf52_image.bat
@@ -3,7 +3,6 @@
 :: ToDo: still need to resolve final build step issues
 ECHO Launch docker container and build NRF52 image
 cd ..
-docker run --rm -it -v %cd%:/usr/morpheus_fw_nrf52 tgage4321/nrfbuild:1.0 
+docker run --rm -it -v %cd%:/usr/morpheus_fw_nrf52 tgage4321/nrfbuild:1.1
 
 pause
- 
