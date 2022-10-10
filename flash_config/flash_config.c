ce3b577326177df30b813baf62e5854c737c4ed6
TylerGageElemind <Tyler.Gage@elemindtech.com>
2022-10-10 08:48:12 -0700

    Update flash config with suggestions from NXP

--- flash_config/flash_config.c
+++ flash_config/flash_config.c
@@ -31,11 +31,11 @@ const flexspi_nor_config_t flexspi_config = {
 
 			.waitTimeCfgCommands = 1,
 
-			.controllerMiscOption = (1u << kFlexSpiMiscOffset_SafeConfigFreqEnable),
+			//.controllerMiscOption = (1u << kFlexSpiMiscOffset_SafeConfigFreqEnable),
 
             .deviceType    = 0x1,
             .sflashPadType = kSerialFlash_4Pads,
-            .serialClkFreq = kFlexSpiSerialClk_SDR_48MHz,
+            .serialClkFreq = kFlexSpiSerialClk_SDR_24MHz,
             .sflashA1Size  = 0,
             .sflashA2Size  = 0,
             .sflashB1Size  = 0x1000000,
@@ -44,13 +44,7 @@ const flexspi_nor_config_t flexspi_config = {
                 {
                     /* Read */
                     [0] = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x6B, RADDR_SDR, FLEXSPI_1PAD, 0x18),
-                    [1] = FLEXSPI_LUT_SEQ(DUMMY_SDR, FLEXSPI_1PAD, 0x08, READ_SDR, FLEXSPI_4PAD, 0x04),
-
-                    /* Read Status */
-                    [4 * 1 + 0] = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x05, READ_SDR, FLEXSPI_1PAD, 0x04),
-
-                    /* Write Enable */
-                    [4 * 3 + 0] = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x06, STOP_EXE, FLEXSPI_1PAD, 0x00),
+                    [1] = FLEXSPI_LUT_SEQ(DUMMY_SDR, FLEXSPI_1PAD, 0x08, READ_SDR, FLEXSPI_4PAD, 0x04)
                 },
         },
     .pageSize           = 0x100,
