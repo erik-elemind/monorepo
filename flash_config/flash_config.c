dfb8487bc10643d9217c882f8b21d25c93746d60
TylerGageElemind <Tyler.Gage@elemindtech.com>
2022-10-26 13:18:17 -0700

    Clean up flash config block

--- flash_config/flash_config.c
+++ flash_config/flash_config.c
@@ -38,17 +38,9 @@ const flexspi_nor_config_t flexspi_config = {
             .sflashB2Size  = 0,
             .lookupTable =
                 {
-                    //[0] = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x03, RADDR_SDR, FLEXSPI_1PAD, 0x18),
-					//[1] = FLEXSPI_LUT_SEQ(READ_SDR, FLEXSPI_1PAD, 0x04, STOP_EXE, FLEXSPI_1PAD, 0),
 					[0] = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0xEB, RADDR_SDR, FLEXSPI_4PAD, 0x18),
 					[1] = FLEXSPI_LUT_SEQ(MODE8_SDR, FLEXSPI_4PAD, 0xFF, DUMMY_SDR, FLEXSPI_4PAD, 0x04),
 					[2] = FLEXSPI_LUT_SEQ(READ_SDR, FLEXSPI_4PAD, 0x04, STOP_EXE, FLEXSPI_1PAD, 0x00)
-
-					/* Read Status*/
-					//[4*1+0] = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x05, READ_SDR, FLEXSPI_1PAD, 0x04),
-
-					/* Write Enable*/
-					//[4*3+0] = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x06, STOP_EXE, FLEXSPI_1PAD, 0x00),
                 },
         },
     .pageSize           = 0x100,
