c749652b1d9103ec42a4fb1035c1c613faa67227
beb <florence.lee@elemindtech.com>
2023-06-01 10:59:39 -0400

    3 channel accel

--- source/ml/ml.cpp
+++ source/ml/ml.cpp
@@ -406,8 +406,6 @@ static void handle_state_standby(ml_event_t *event)
   }
 }
 
-// Should we include events for configuration like in eeg_processor.cpp?
-// Depends on how often we plan to live reconfigure the preprocessing pipeline 
 static void handle_state_preprocess_data(ml_event_t *event)
 {
   switch (event->type) {
@@ -420,41 +418,30 @@ static void handle_state_preprocess_data(ml_event_t *event)
 			// 2nd order Butterworth Filter
 			for (int i; i < ACCEL_BUF_SIZE; i++){ 
 				g_accelx_ready_p[i] = g_accel_filt.filter(g_accelx_ready_p[i]); // feed values in one at a time
-				// g_accely_ready_p[i] = g_accel_filt.filter(g_accely_ready_p[i]); // check eeg filt again to see how david advances the buffer
-				// g_accelz_ready_p[i] = g_accel_filt.filter(g_accelz_ready_p[i]);
+				g_accely_ready_p[i] = g_accel_filt.filter(g_accely_ready_p[i]);
+				g_accelz_ready_p[i] = g_accel_filt.filter(g_accelz_ready_p[i]);
 			}
 
 			// Upsampling to 125 Hz -> make these defines
 			int fs_orig = 25; // Hz
 			int fs_new = 125; // Hz
 			
-			// Cast to vector to resample
-			// first vector. at beginning of alloc_buf
-			// previous and current block sizes are 0, is first block
-			// left and right child is just this block (0015A6C8)
-
-			// need to allocate new block
-			// allocates 0x0EB0 bytes, 3760 (for 8 byte boundary)
 			std::vector<float, bufferAllocator<float>> accelx_vec(g_accelx_ready_p, g_accelx_ready_p + ACCEL_BUF_SIZE);
-			// std::vector<float, bufferAllocator<float>> accely_vec(g_accely_ready_p, g_accely_ready_p + ACCEL_BUF_SIZE);
-			// std::vector<float, bufferAllocator<float>> accelz_vec(g_accelz_ready_p, g_accelz_ready_p + ACCEL_BUF_SIZE);
+			std::vector<float, bufferAllocator<float>> accely_vec(g_accely_ready_p, g_accely_ready_p + ACCEL_BUF_SIZE);
+			std::vector<float, bufferAllocator<float>> accelz_vec(g_accelz_ready_p, g_accelz_ready_p + ACCEL_BUF_SIZE);
 
-			std::vector<float, bufferAllocator<float>> accelx_vec_out; // lets see where this starts
-//			accelx_vec_out.reserve(ACCEL_BUF_SIZE);
+			std::vector<float, bufferAllocator<float>> accelx_vec_out;
+			std::vector<float, bufferAllocator<float>> accely_vec_out;
+			std::vector<float, bufferAllocator<float>> accelz_vec_out;
 
-			// std::vector<float, bufferAllocator<float>> accely_vec_out;
-			// std::vector<float, bufferAllocator<float>> accelz_vec_out;
-
-			// the output signal reserve in resample (and prob insert too)is trying to access a block within accelx_vec_out
-			// accelx_vec_out stops at 0x15e180
 
 			resample<float> (fs_orig, fs_new, accelx_vec, accelx_vec_out);
-			// resample<float> (fs_orig, fs_new, accely_vec, accely_vec);
-			// resample<float> (fs_orig, fs_new, accelz_vec, accelz_vec);
+			resample<float> (fs_orig, fs_new, accely_vec, accely_vec);
+			resample<float> (fs_orig, fs_new, accelz_vec, accelz_vec);
 
 			accelx_vec = z_score_normalize(accelx_vec);
-			// accely_vec = z_score_normalize(accely_vec);
-			// accelz_vec = z_score_normalize(accelz_vec);
+			accely_vec = z_score_normalize(accely_vec);
+			accelz_vec = z_score_normalize(accelz_vec);
 
 			g_accel_process_done = true;
 
