4684ed5168b64ca8f5f563d76ba49abac1f5d754
TylerGageElemind <Tyler.Gage@elemindtech.com>
2023-06-06 09:38:29 -0700

    Update USB programming script
    Erase command only needs erase all command, we dont need to split it up by region

--- scripts/program_nxp_over_usb_v1_0.bat
+++ scripts/program_nxp_over_usb_v1_0.bat
@@ -13,8 +13,7 @@ ECHO ================ Programming "%filename%" over USB ================
 blhost -t 50000 -u 0x1FC9,0x0020 -- fill-memory 0x1C000 4 0xC1000204 word
 blhost -t 50000 -u 0x1FC9,0x0020 -- fill-memory 0x1C004 4 0x20000000 word
 blhost -t 50000 -u 0x1FC9,0x0020 -- configure-memory 9 0x1C000
-blhost -t 50000 -u 0x1FC9,0x0020 -- flash-erase-region 0x8000000 0x400000
-blhost -t 50000 -u 0x1FC9,0x0020 -- flash-erase-region 0x8400000 0x400000
+blhost -t 50000 -u 0x1FC9,0x0020 -- flash-erase-all 9
 blhost -t 50000 -u 0x1FC9,0x0020 -- write-memory 0x8000000 %filename%
 blhost -t 50000 -u 0x1FC9,0x0020 -- reset 
 ECHO ================ Done ================
