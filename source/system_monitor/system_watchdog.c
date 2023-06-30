81ed61e89d2a78adcb2727a11bd950136e1424f3
Erik <erik.su@elemindtech.com>
2023-06-30 11:50:36 -0400

    MFW-329: enable memfault coredumps on watchdog timeouts

--- source/system_monitor/system_watchdog.c
+++ source/system_monitor/system_watchdog.c
@@ -12,35 +12,7 @@
 
 
 #define APP_WDT_IRQn        WDT0_IRQn
-#define APP_WDT_IRQ_HANDLER WDT_BOD_IRQHandler
 #define WDT_CLK_FREQ        CLOCK_GetWdtClkFreq(0)
-
-
-void APP_WDT_IRQ_HANDLER(void)
-{
-    uint32_t wdtStatus = WWDT_GetStatusFlags(WWDT0);
-
-//    APP_LED_TOGGLE;
-
-    /* The chip will reset before this happens */
-    if (wdtStatus & kWWDT_TimeoutFlag)
-    {
-        WWDT_ClearStatusFlags(WWDT0, kWWDT_TimeoutFlag);
-    }
-
-    /* Handle warning interrupt */
-    if (wdtStatus & kWWDT_WarningFlag)
-    {
-        /* A watchdog feed didn't occur prior to warning timeout */
-        WWDT_ClearStatusFlags(WWDT0, kWWDT_WarningFlag);
-        /* User code. User can do urgent case before timeout reset.
-         * IE. user can backup the ram data or ram log to flash.
-         * the period is set by config.warningValue, user need to
-         * check the period between warning interrupt and timeout.
-         */
-    }
-    SDK_ISR_EXIT_BARRIER;
-}
 
 
 void system_watchdog_init(){
@@ -59,7 +31,7 @@ void system_watchdog_init(){
   wdtFreq = WDT_CLK_FREQ / 4;
 
   /*
-   * Set watchdog feed time constant to approximately 120s
+   * Set watchdog pet time constant to approximately 30s
    * Set watchdog warning time to 512 ticks after feed time constant
    * Set watchdog window time to 1s
    */
@@ -67,8 +39,8 @@ void system_watchdog_init(){
   config.warningValue = 512;
 //  config.windowValue  = wdtFreq * 1;
   config.windowValue  = 0xFFFFFF; // disable reset on early feed
-  /* Configure WWDT to reset on timeout */
-  config.enableWatchdogReset = true;
+  /* Configure WWDT to not reset on timeout for Memfault handling instead*/
+  config.enableWatchdogReset = false;
   /* Setup watchdog clock frequency(Hz). */
   config.clockFreq_Hz = WDT_CLK_FREQ;
   WWDT_Init(WWDT0, &config);
@@ -76,13 +48,8 @@ void system_watchdog_init(){
   NVIC_EnableIRQ(APP_WDT_IRQn);
 }
 
-void system_watchdog_feed(){
+void system_watchdog_pet(){
   WWDT_Refresh(WWDT0);
-}
-
-
-uint32_t system_watchdog_reset_ms(){
-  return 5000;
 }
 
 
