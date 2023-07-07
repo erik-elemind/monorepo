cad39174623ca25cb682302f5395954d827fbbad
Erik <erik.su@elemindtech.com>
2023-07-07 12:11:24 -0400

    MFW-197: build NXP image using MCUXpresso headless CLI

---
+++ scripts/build_imxrt685_image.bat
@@ -0,0 +1,29 @@
+REM Batch file to build an Eclipse project from the command line on Windows
+REM Example for using MCUXpresso IDE
+
+@REM path of the IDE
+SET IDE_PATH=C:\nxp\MCUXpressoIDE_11.6.1_8255
+
+@REM path to GNU tools and compiler: arm-none-eabi-gcc.exe, ....
+SET TOOLCHAIN_PATH=%IDE_PATH%\ide\tools\bin
+
+@REM variable to the command line Eclipse IDE executable
+SET IDE=%IDE_PATH%\ide\mcuxpressoidec.exe
+
+ECHO Extending PATH if not already present
+ECHO %PATH%|findstr /i /c:"%TOOLCHAIN_PATH:"=%">nul || set PATH=%PATH%;%TOOLCHAIN_PATH%
+
+SET WORKSPACE_PATH=C:\Users\Erik\Documents\work\m33_workspaces\developer
+ECHO Launching Eclipse IDE
+
+@REM -build on project: normal build, does a (re-)build of all targets (Release, Debug): the fact that it does a full build sounds lika an issue in Eclipse CDT 9.4.3?
+"%IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data %WORKSPACE_PATH% -build morpheus_fw_imxrt685/Debug
+
+@REM - build on a build target: only builds the target, this works properly with CDT 9.4.3
+@REM "%IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "c:\tmp\wsp" -build MyProject/Debug
+
+@REM -cleanbuild on project: this does a 'clean' on each build target followed by a build
+@REM "%IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "c:\tmp\wsp" -cleanBuild MyProject
+
+@REM -cleanbuild on a build target: this does a 'clean' only on the build target, no build
+@REM "%IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data "c:\tmp\wsp" -cleanBuild MyProject/Debug
